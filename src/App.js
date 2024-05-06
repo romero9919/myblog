aa448cd9a793b8d7803281f2dfdea8a28457d625
romero9919 <hojoong9919@naver.com>
2024-05-06 20:36:30 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import "./index.css";
+import Header from './pages/Header';
+import Home from './pages/Home';
+import About from './pages/About';
+import Community from './pages/Community';
+import Contact from './pages/Contact';
+import Doodle from './pages/Doodle';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+         <Header />
+         <Routes>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="doodle" element={<Doodle />} />
+            <Route path="community" element={<Community />} />
+         </Routes>
+       </BrowserRouter>
+    </>
   );
-}
+};
 
 export default App;
